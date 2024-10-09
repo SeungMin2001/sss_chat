8e224f11eeb2a6a0a1931e314878ce53aa5936d0
SeungMin <75976324+SeungMin2001@users.noreply.github.com>
2024-10-10 01:42:52 +0900

    good

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require("express")
 const { WebSocketServer } = require("ws")
 const app = express()
+
 app.use(express.static("public"))
 
-const PORT = process.env.PORT || 8000; // 8000은 로컬 개발용 기본 포트
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
-
+app.listen(8000, () => {
+  console.log(`Example app listening on port 8000`)
+})
 
 const wss = new WebSocketServer({ port: 8001 })
 
+// HTTP 서버 생성
+//const server = http.createServer(app);
+
+// HTTP 서버와 동일한 포트로 WebSocket 서버 실행
+
+
 wss.broadcast = (message) => {
     wss.clients.forEach((client) => {
-      client.send(message);
+      if (client.readyState === client.OPEN) {
+        client.send(message);
+      }
     });
-  };
+};
   
-  wss.on("connection", (ws, request) => {
-    // 새로운 클라이언트가 연결되었을 때 메시지 전송
+wss.on("connection", (ws, request) => {
     const welcomeMessage = `새로운 유저가 접속했습니다. 현재 유저 ${wss.clients.size} 명`;
     wss.clients.forEach(client => {
         if (client.readyState === client.OPEN) {
@@ -26,7 +32,6 @@ wss.broadcast = (message) => {
         }
     });
 
-    // 메세지 수신
     ws.on("message", (data) => {
         wss.clients.forEach(client => {
             if (client.readyState === client.OPEN) {
@@ -35,7 +40,6 @@ wss.broadcast = (message) => {
         });
     });
 
-    // 클라이언트가 연결을 종료할 때
     ws.on("close", () => {
         const goodbyeMessage = `유저 한명이 떠났습니다. 현재 유저 ${wss.clients.size} 명`;
         wss.clients.forEach(client => {
@@ -47,3 +51,5 @@ wss.broadcast = (message) => {
 
     console.log(`새로운 유저 접속: ${request.socket.remoteAddress}`);
 });
+
+
